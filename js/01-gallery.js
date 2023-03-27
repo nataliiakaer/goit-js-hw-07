171090d9dfaf4de0a569e73fa740ad4710f7966f
Nataliia Kruhliak <nataliia.kaer@gmail.com>
2023-03-27 06:49:18 +0300

    added markup img

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector(".gallery");
+const imageMarkup = createImageItemMurkup(galleryItems);
+
+galleryListEl.insertAdjacentHTML("beforeend", imageMarkup);
+galleryListEl.addEventListener("click", onImageClick);
+
+function createImageItemMurkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      // console.log(preview);
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join("");
+}
+
+function onImageClick(evt) {
+  const isSelectedImageEl = evt.target.classList.contains("gallery__image");
+
+  if (!isSelectedImageEl) {
+    return;
+  }
+  console.log(evt.target);
+}
